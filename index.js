31a51b419f666f2559bb1b5cb9dcc48992aebfe2
Attacktive <66462458+Attacktive@users.noreply.github.com>
2025-09-06 22:03:01 +0900

    Refactor/tweaks (#22)
    * feature: add 'viewport' meta
    
    * refactor: improve HTML markup
    
    * refactor: improve styles
    
    * feature: make 'xEvaluatedTo' reactive
    
    * refactor: remove trailing whitespaces
    
    * feature: improve styles around input
    
    * feature: update 'xEvaluatedTo' on load
    
    * feature: add .editorconfig
    
    * refactor: reformat code
    
    * refactor: reformat code
    
    * refactor: use 'context' media feature range notation
    
    * refactor: print error to console when caught

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
-document.addEventListener("DOMContentLoaded", onBodyLoad);
+document.addEventListener('DOMContentLoaded', onBodyLoad);
 
-const runButton = document.querySelector("#run");
-runButton.addEventListener("click", run);
+const runButton = document.querySelector('#run');
+runButton.addEventListener('click', run);
 
 function onBodyLoad() {
-	const footer = document.querySelector("#user-agent");
+	const footer = document.querySelector('#user-agent');
 	footer.textContent = window.navigator.userAgent;
 
-	const input = document.querySelector("#input");
+	const input = document.querySelector('#input');
 
-	input.addEventListener("input", onInput);
+	input.addEventListener('input', onInput);
 	input.addEventListener(
-		"keydown",
+		'keydown',
 		(event) => {
-			if (event.ctrlKey && event.code === "Enter") {
+			if (event.ctrlKey && event.code === 'Enter') {
 				run();
 			}
 		}
 	);
+
+	onInput();
 }
 
 function onInput() {
-	const input = document.querySelector("#input");
-	const button = document.querySelector("#run");
+	const input = document.querySelector('#input');
+	const button = document.querySelector('#run');
+	const xEvaluatedTo = document.querySelector('#x-evaluated-to');
 
 	if (input.value.length > 0) {
-		button.removeAttribute("disabled");
+		button.removeAttribute('disabled');
+
+		const toEval = `x = ${input.value}`;
+		let x = undefined;
+
+		try {
+			eval(toEval);
+			xEvaluatedTo.textContent = `const x = ${format(x)};`;
+		} catch (error) {
+			console.error(error);
+
+			xEvaluatedTo.textContent = 'Invalid expression';
+		}
 	} else {
-		button.setAttribute("disabled", "");
+		button.setAttribute('disabled', '');
+		xEvaluatedTo.textContent = 'undefined';
 	}
 }
 
 function run() {
-	const input = document.querySelector("#input");
-	const xEvaluatedTo = document.querySelector("#x-evaluated-to");
-	const output = document.querySelector("#output");
+	const input = document.querySelector('#input');
+	const xEvaluatedTo = document.querySelector('#x-evaluated-to');
+	const output = document.querySelector('#output');
+	const button = document.querySelector('#run');
+	const buttonText = button.querySelector('.button-text');
+	const spinner = button.querySelector('.spinner-border');
+
+	button.disabled = true;
+	buttonText.classList.add('d-none');
+	spinner.classList.remove('d-none');
 
 	const toEval = `x = ${input.value}`;
-	console.debug("toEval", toEval);
+	console.debug('toEval', toEval);
 
 	let x = undefined;
 	let result;
@@ -53,22 +76,37 @@ function run() {
 			if (isInfinite) {
 				result = examples
 					.map((example, index) => `x == ${format(example, index)}`)
-					.join("\n")
-					.concat("\n…");
+					.join('\n')
+					.concat('\n…');
 			} else {
 				result = examples
 					.map(example => `x == ${format(example)}`)
-					.join("\n");
+					.join('\n');
 			}
+
+			output.classList.add('success');
 		} else {
 			result = `Nothing is loosely equal to ${format(x)}.`;
 		}
+
+		output.classList.remove('error');
 	} catch (error) {
 		console.error(error);
 
-		xEvaluatedTo.textContent = "undefined";
-		result = error.stack;
+		xEvaluatedTo.textContent = 'undefined';
+		result = error.message || error.stack;
+		output.classList.add('error');
+		output.classList.remove('success');
 	}
 
 	output.textContent = result;
+
+	setTimeout(
+		() => {
+			button.disabled = false;
+			buttonText.classList.remove('d-none');
+			spinner.classList.add('d-none');
+		},
+		100
+	);
 }
